7c7d53ea613f63955de3452ae645752afb6527bd
Resi Respati <resir014@gmail.com>
2015-11-27 22:24:31 +0700

    Restructured folders, major updates to build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,64 +5,110 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    // Project configuration
+    pkg: grunt.file.readJSON('package.json'),
 
-    autoprefixer: {
+    // Compile Sass files
+    sass: {
+      options: {
+        precision: 6,
+        sourceComments: false,
+        outputStyle: 'compressed'
+      },
       build: {
-        files: [{
-          expand: true,
-          cwd: 'app/_sass/',
-          src: ['components/*.scss'],
-          dest: 'app/_sass/'
-        }]
-      }
-    },
-
-    browserSync: {
-      serve: {
-        options: {
-          watchTask: true,
-          server: {
-            baseDir: 'app'
-          }
-        },
-        bsFiles: {
-          src: ['app/*.html', 'app/css/*.css']
+        files: {
+          'src/css/main.css': 'src/scss/main.scss'
         }
       }
     },
 
+    // Clean up CSS
     csscomb: {
+      options: {
+        config: '.csscomb.json'
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['css/main.css'],
+          dest: 'src/'
+        }]
+      }
+    },
+
+    // Handle vendor prefixing
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({ browsers: ['last 2 versions', 'ie 8', 'ie 9'] })
+        ]
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['css/main.css'],
+          dest: 'src/'
+        }]
+      }
+    },
+
+    // Optimise/minify images
+    imagemin: {
       build: {
         options: {
-          config: '.csscomb.json'
+          optimizationLevel: 3,
+          progressive: true,
+          interlaced: true,
+          svgoPlugins: [{ removeViewBox: false }]
         },
         files: [{
           expand: true,
-          cwd: 'app/_sass/',
-          src: ['components/*.scss'],
-          dest: 'app/_sass/'
+          cwd: 'src',
+          src: ['img/**/*.{png,jpg,gif}'],
+          dest: 'src'
         }]
+      },
+    },
+
+    // Builds a complete site to the `dist` folder
+    copy: {
+      deploy: {
+        expand: true,
+        cwd: 'src/',
+        src: ['**/*', '!**/scss/**'],
+        dest: 'dist/'
+      },
+    },
+
+    // Cleans up the `dist` folder
+    clean: {
+      deploy: {
+        src: 'dist'
       }
     },
 
-    sass: {
-      build: {
+    // Starts a local server with LiveReload instance
+    connect: {
+      server: {
         options: {
-          sourcemap: 'none',
-          style: 'compressed'
-        },
-        files: [{
-          src: 'app/_sass/main.scss',
-          dest: 'app/css/main.css'
-        }]
+          livereload: true,
+          open: true,
+          base: 'src/',
+          hostname: '*',
+          port: 9001
+        }
       }
     },
 
+    // Watch for changes in files
     watch: {
+      options: {
+        livereload: true
+      },
       serve: {
-        files: [
-          'app/*.html', 'app/_sass/**/*.scss'
-        ],
+        files: ['src/**/*'],
         tasks: ['build']
       }
     }
@@ -70,11 +116,14 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('build', [
-    'autoprefixer:build', 'csscomb:build', 'sass:build'
+    'sass', 'postcss', 'imagemin'
   ]);
 
-  // browsersync task
-  grunt.registerTask('serve', ['browserSync', 'watch']);
+  // server task
+  grunt.registerTask('serve', ['build', 'connect', 'watch']);
+
+  // deploy task
+  grunt.registerTask('deploy', ['build', 'clean', 'copy:deploy']);
 
   // default task
   grunt.registerTask('default', 'build');
